ee42ae89a604b04743e8def25d4608a88068e209
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-26 23:35:08 +0900

    条件演算子

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -17,17 +17,22 @@ public class Test : MonoBehaviour
         Vector3 threeVector = new Vector3(1, 5.6f, 3.6f);
         print(threeVector);
 
-        // 条件演算子
+        // if文
+        // int age = 20;
+        // string result;
+        // if(age >= 20)
+        // {
+        //     result ="大人";
+        // }
+        // else
+        // {
+        //     result = "子供";
+        // }
+        // Debug.Log(result);
+        
+        //条件演算子
         int age = 20;
-        string result;
-        if(age >= 20)
-        {
-            result ="大人";
-        }
-        else
-        {
-            result = "子供";
-        }
+        string result = age >= 20 ? "大人" : "子供" ;
         Debug.Log(result);
     }
 
