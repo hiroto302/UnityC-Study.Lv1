61dedca927c733d737fbdedf73a3b3cf598523bb
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-26 23:30:03 +0900

    変数の型、条件演算子

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -8,6 +8,27 @@ public class Test : MonoBehaviour
     void Start()
     {
         print("Hello, World");
+
+        // 変数の型
+        // 真偽値
+        bool boolean = true;
+        print("真偽値:" + boolean);
+        //三次元ベクトル
+        Vector3 threeVector = new Vector3(1, 5.6f, 3.6f);
+        print(threeVector);
+
+        // 条件演算子
+        int age = 20;
+        string result;
+        if(age >= 20)
+        {
+            result ="大人";
+        }
+        else
+        {
+            result = "子供";
+        }
+        Debug.Log(result);
     }
 
     // Update is called once per frame
