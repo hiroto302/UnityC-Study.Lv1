838644ff0b93c81b5c73ccf0e6d0810815f3c778
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-27 13:42:53 +0900

    型推論
    ローカル変数でのみ使用可能

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        // 型推論 ローカル変数でのみ使用可能
+        // 代入される右側の形式により予測された型が入る
+        var hp = 100;
+        var st = "hello";
+        var c = new Vector3(1, 2, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
