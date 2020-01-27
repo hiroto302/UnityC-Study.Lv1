3a28d9499a92b6fa50811d38f7cfa861eba08f4f
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-27 15:23:41 +0900

    Switch文
    処理の変化

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    // Switch分 値により行う処理を変化
+
+    void Start()
+    {
+        int playerLevel = 10;
+        GetSkill(playerLevel);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GetSkill(int lv)
+    {
+        switch(lv)
+        {
+            case 5:
+                print("ひのこ");
+                break;
+            case 10:
+                print("電光石火");
+                break;
+        }
+        
+    }
+
+
+}
