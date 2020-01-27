7de1f8f4e76bc7f34e1dbd624d21ee972356f679
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-27 14:35:08 +0900

    配列
    Array
    型[] 変数名 = new 型[要素の数]
    変数名[0] = 1;
    型[] 変数名 = {1,2,3}
    Debug.Log(変数名[0])

---
+++ Assets/Scripts/Array.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+    //配列 複数の値を格納ができる
+    void Start()
+    {
+        int[] hps = new int[3];  //配列の型を指定し、[]の中に格納したい要素の数を指定
+        hps[0] = 10; //配列に要素を格納  [0]とすることで配列の1つ目に10を格納する
+
+        int[] mps = {1, 2, 3};  //配列の型の要素を同時に指定  自動的に格納する分の箱を作成してくれる
+        Debug.Log(mps[1]);  //2が出力される
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/forTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forTest : MonoBehaviour
+{
+    void Start()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            print("数値：" + i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
