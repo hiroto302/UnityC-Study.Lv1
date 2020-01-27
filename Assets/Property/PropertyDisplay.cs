a3ebff4f367de805101e4a952b1346b0f4dc9ac7
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-27 15:52:11 +0900

    プロパティー
    他のクラスで、変数の読み取りｍ書き換えを設定する

---
+++ Assets/Property/PropertyDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertyDisplay : MonoBehaviour
+{
+    public PropertyPlayer propertyPlayer;
+    void Start()
+    {
+        print(propertyPlayer.hp);
+        propertyPlayer.hp = 30;  //pubicだと他のクラスの変数を書き換えてしますことが出来てしまう、そこでプロパティーを使用する
+
+        print(propertyPlayer.Hp);
+        // propertyPlayer.Hp = 30;  //変数を変えようとするとerrorになり、プロパティーが効いていることがわかる
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Property/PropertyPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertyPlayer : MonoBehaviour
+{
+    // private int hp = 100;  //デフォルトだとprivateになっている
+    public int hp = 100;
+
+    // プロパティー
+    // プロパティーを使用する場合、凡例として変数の頭文字は大文字
+    // get; 読み取り  set; 書き込み
+    // private setとすることで、他のクラスでは書き込みをすることが出来ないように指定することができる
+    public int Hp { get; private set;} = 200;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
