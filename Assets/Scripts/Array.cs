5a7fc03a73fc8fa2d4574a99b3725b0714734fbc
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-27 15:11:20 +0900

    List
    配列への要素の追加

--- Assets/Scripts/Array.cs
+++ Assets/Scripts/Array.cs
@@ -12,6 +12,32 @@ public class Array : MonoBehaviour
 
         int[] mps = {1, 2, 3};  //配列の型の要素を同時に指定  自動的に格納する分の箱を作成してくれる
         Debug.Log(mps[1]);  //2が出力される
+
+        //foreach
+        int[] nums = {4, 5, 6};
+        foreach(var num in nums)  //配列の全ての要素に下記の処理を行う
+        {
+            print(num);
+        }
+
+        //List  配列への要素の追加
+        // List<int> nums = new List<int>();
+        // var numbers = new List<int>();
+        // numbers.Add(7);  要素の追加
+        // numbers.Add(8);
+        // numbers.Add(9);
+        // print(numbers[1]);
+        // foreach(var count in numbers)
+        // {
+        //     print(count);
+        // }
+
+        // Listに初期値を与える
+        List<int> numbers = new List<int>(){7, 8, 9};
+        foreach(var count in numbers)
+        {
+            print(count);
+        }
     }
 
     // Update is called once per frame
