b90a8fae6c0d9cf1b492d0d3ca22e4dc891de8c4
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-27 13:33:28 +0900

    スコープ Scope
    グローバル変数
    ローカル変数

---
+++ Assets/Scope.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scope : MonoBehaviour
+{
+    //グローバル変数  クラスの波括弧の中に記述  このクラス内であればどこででも使用できる
+    int hp = 100;
+
+    void Start()
+    {
+        Debug.Log(hp);
+
+        //ローカル変数  このメソッド内でのみ使用可能
+        int mp = 300;
+
+        if(mp == 300)
+        {
+            int a = 3000;  //このようにif文の中で作成した変数は、そのif文のみで使用可能
+            Debug.Log(a);  //errorにならない
+        }
+
+        // Debug.Log(a);  errorになる
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
